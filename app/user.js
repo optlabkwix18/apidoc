36c4efeeceee0e7f7f83fa40562b4c210b741ab9
PW486 <ooqwe486@gmail.com>
2018-03-19 22:22:06 +0900

    Fix user create

--- app/user.js
+++ app/user.js
@@ -1,18 +1,21 @@
 /**
- * @api {get} /user/:id 회원가입에 필요한 ID이다.
+ * @api {get} /user/:id Request User information
  * @apiName GetUser
  * @apiGroup User
  *
- * @apiParam {String} id Users unique ID.
+ * @apiParam {Number} id Users unique ID.
  *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *     {
- *       "id": "정가빈안녕"
+ *       "firstname": "John",
+ *       "lastname": "Doe"
  *     }
  *
- * @apiError UserNotFound User ID를 찾을 수 없습니다.
+ * @apiError UserNotFound The id of the User was not found.
  *
  * @apiErrorExample Error-Response:
  *     HTTP/1.1 404 Not Found
@@ -20,3 +23,31 @@
  *       "error": "UserNotFound"
  *     }
  */
+
+ /**
+  * @api {post} /user Request User Create
+  * @apiName PostUser
+  * @apiGroup User
+  *
+  * @apiParam {String} email user email
+  * @apiParam {String} firstname firstname
+  * @apiParam {String} lastname lastname
+  *
+  * @apiSuccess {String} id user의 고유번호
+  * @apiSuccess {String} email 유저의 이메일
+  *
+  * @apiSuccessExample Success-Response:
+  *     HTTP/1.1 200 OK
+  *     {,
+  *       "id": "23194",
+  *       "email": "ooee@gmail.com"
+  *     }
+  *
+  * @apiError UserNotCreated 회원가입 실패했을 때
+  *
+  * @apiErrorExample Error-Response:
+  *     HTTP/1.1 404 Not Found
+  *     {
+  *       "error": "UserNotCreated"
+  *     }
+  */
